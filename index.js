97ea3c32628335b31a22333e2ed25da18e2637bd
shahid-ahmed1 <shahidcse2122gmail,com>
2025-03-05 02:41:39 +0600

    added a review details data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express= require('express')
 const cors = require ('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -30,6 +30,13 @@ async function run() {
       .toArray();
        res.send(result)
     })
+
+    app.get('/review/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }; 
+      const review = await reviewCollection.findOne(query);
+      res.send(review);
+  });
 
     app.post('/review',async(req,res)=>{
     const newReview = req.body;
