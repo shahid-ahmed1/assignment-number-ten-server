09717f0aa600543868c5547ec2146ecab1411d29
shahid-ahmed1 <shahidcse2122gmail,com>
2025-03-05 23:29:13 +0600

    complete allReview red

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+
 
 
 const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_password}@cluster0.nukrg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -22,6 +23,9 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     const reviewCollection = client.db('reviewDB').collection('review')
+
+    
+      
     app.get('/review',async(req,res)=>{
       const result = await reviewCollection
       .find({})
@@ -30,6 +34,13 @@ async function run() {
       .toArray();
        res.send(result)
     })
+    
+   
+    app.get('/review/all',async(req,res)=>{
+      const cursor = reviewCollection.find();
+      const result= await cursor.toArray();
+      res.send(result)
+    })
 
     app.get('/review/:id', async (req, res) => {
       const id = req.params.id;
@@ -37,6 +48,7 @@ async function run() {
       const review = await reviewCollection.findOne(query);
       res.send(review);
   });
+ 
 
     app.post('/review',async(req,res)=>{
     const newReview = req.body;
